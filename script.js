3ceb32ffa15da77c4c6a256e129a2942fae26e11
strangecarr <tomcarruk@gmail.com>
2021-09-22 20:12:20 +0100

    big edits

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ function compute() {
   t = parseInt(document.getElementById("years").value);
   si = (p * t * r / 100);
   var n = 2020 + t;
- //Results from campute & highlighting result
-  document.getElementById('rprincipal').innerHTML = "If you deposit <mark>" + p + ".</mark>";
-  document.getElementById('rintrate').innerHTML = "at an interest rate of <mark> " + r + "%" + ".</mark>";
-  document.getElementById('rinterest').innerHTML = "You will receive an amount of <mark> " + si + ".</mark>";
+
+  document.getElementById('rprincipal').innerHTML = "deposit <mark>" + p + ".</mark>";
+  document.getElementById('rintrate').innerHTML = "at an interest of <mark> " + r + "%" + ".</mark>";
+  document.getElementById('rinterest').innerHTML = "You will receive <mark> " + si + ".</mark>";
   document.getElementById( 'rdate').innerHTML = "in the year <mark>" + n + ".</mark>";
 	
 }
-// Update the current slider value (each time you drag the slider handle)								
+
  function update_rate(rate) {
             document.getElementById("percent").innerHTML = rate.value + " %";
         }
 
 
-//validate principal
+
 function validatePrincipal(that){
 if (that.value < 1 ){
-	alert("Enter a Positive number");  //must be greater than or equal to one
+	alert("Enter a Positive number");
 	that.value = " ";
-	that.focus();  //bring focus back to principal if number is incorrect
+	that.focus();
 	}
 }
