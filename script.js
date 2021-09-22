4f4c5f7844834ae950e0d9750d944d8a8f660d30
strangecarr <tomcarruk@gmail.com>
2021-09-22 14:27:20 +0100

    big edits

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  p = parseInt(document.getElementById("principal").value);
+  r = parseFloat(document.getElementById("intrate").value);
+  t = parseInt(document.getElementById("years").value);
+  si = (p * t * r / 100);
+  var n = 2020 + t;
+ //Results from campute & highlighting result
+  document.getElementById('rprincipal').innerHTML = "If you deposit <mark>" + p + ".</mark>";
+  document.getElementById('rintrate').innerHTML = "at an interest rate of <mark> " + r + "%" + ".</mark>";
+  document.getElementById('rinterest').innerHTML = "You will receive an amount of <mark> " + si + ".</mark>";
+  document.getElementById( 'rdate').innerHTML = "in the year <mark>" + n + ".</mark>";
+	
 }
-        
+// Update the current slider value (each time you drag the slider handle)								
+ function update_rate(rate) {
+            document.getElementById("percent").innerHTML = rate.value + " %";
+        }
+
+
+//validate principal
+function validatePrincipal(that){
+if (that.value < 1 ){
+	alert("Enter a Positive number");  //must be greater than or eqqual to one
+	that.value = " ";
+	that.focus();  //bring focus back to principal if number is incorrect
+	}
+}
